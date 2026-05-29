a0845430f3ab5d6a5b006e20cce2d359bc838166
linkboostowner <sdolbin@yandex.ru>
2026-05-29 12:06:24 +0300

    Fix i18n config for missing locale

--- src/i18n/request.js
+++ src/i18n/request.js
@@ -1,5 +1,9 @@
 import { getRequestConfig } from 'next-intl/server';
 
-export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`../../messages/${locale}.json`)).default,
-}));
+export default getRequestConfig(async ({ locale }) => {
+  // Если локаль не определена (например, на странице 404), используем 'en'
+  const safeLocale = locale || 'en';
+  return {
+    messages: (await import(`../../messages/${safeLocale}.json`)).default,
+  };
+});
